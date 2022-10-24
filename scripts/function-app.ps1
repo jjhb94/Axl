1f7cd0218f8dddc2f4f60b926e12f9779ed0a87c
Jesse Baker <jjhb94@gmail.com>
2022-10-24 16:31:01 -0500

    Updating pshell file

--- scripts/function-app.ps1
+++ scripts/function-app.ps1
@@ -5,26 +5,26 @@ az group create --name rgaxl0001dev --location eastus
 ## Validate deployment and parameters
 az deployment group validate \
 --resource-group rgaxl0001dev \
---template-file function-app.json \
+--template-file function-app.bicep \
 --parameters @function-app.parameters.json
 
-az deployment group validate --resource-group rgaxl0001dev --template-file function-app.json --parameters @function-app.parameters.json
+az deployment group validate --resource-group rgaxl0001dev --template-file function-app.bicep --parameters @function-app.parameters.json
 
 
 ## Review the deployment without deploying
 az deployment group what-if \
 --name AxlFunctionAppDeployment \
 --resource-group rgaxl0001dev \
---template-file function-app.json \
+--template-file function-app.bicep \
 --parameters @function-app.parameters.json
 
-az deployment group what-if --name AxlFunctionAppDeployment --resource-group rgaxl0001dev --template-file function-app.json --parameters @function-app.parameters.json
+az deployment group what-if --name AxlFunctionAppDeployment --resource-group rgaxl0001dev --template-file function-app.bicep --parameters @function-app.parameters.json
 
 ## Create a new deployment
 az deployment group create \
 --name AxlFunctionAppDeployment \
 --resource-group $resourcegroup \
---template-file function-app.json \
+--template-file function-app.bicep \
 --parameters @function-app.parameters.json
 
-az deployment group create --name AxlFunctionAppDeployment --resource-group rgaxl0001dev --template-file function-app.json --parameters @function-app.parameters.json
+az deployment group create --name AxlFunctionAppDeployment --resource-group rgaxl0001dev --template-file function-app.bicep --parameters @function-app.parameters.json
